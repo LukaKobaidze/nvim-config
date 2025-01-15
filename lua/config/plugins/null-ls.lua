acd9394072d89f5138c247d107e8d54cf2e17e00
LukaKobaidze <lukakobaidze.dev@gmail.com>
2025-01-15 07:25:31 +0400

    update

--- lua/config/plugins/null-ls.lua
+++ lua/config/plugins/null-ls.lua
@@ -11,6 +11,9 @@ return {
         null_ls.builtins.formatting.prettier.with({
           filetypes = { "css", "sass", "scss", "javascript", "typescript", "css", "html", "javascriptreact", "typescriptreact" }
         }),
+        null_ls.builtins.formatting.stylua.with({
+          command = '/home/luka/.cargo/bin/stylua'
+        })
       },
     })
     vim.api.nvim_set_keymap("n", "<leader>fr", ":lua vim.lsp.buf.format({ async = true })<CR>", { noremap = true, silent = true })
