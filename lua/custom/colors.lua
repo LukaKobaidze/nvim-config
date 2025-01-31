213e99a7a80b4d141519fd459048cb82c010cd51
LukaKobaidze <lukakobaidze.dev@gmail.com>
2025-01-31 16:02:49 +0400

    fixed error

--- lua/custom/colors.lua
+++ lua/custom/colors.lua
@@ -6,21 +6,21 @@ vim.api.nvim_set_option("pumblend", 0)
 vim.api.nvim_set_hl(0, "Normal", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "NONE" })
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" }) -- Transparent background
-vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = border }) -- Border color
-vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = border })
-vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = border })
-vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = border, bg = "none" })
+vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg = primary }) -- Border color
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = primary })
+vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = primary })
+vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = primary, bg = "none" })
 vim.api.nvim_set_hl(0, "TelescopeSelection", { fg = "#ffffff", bg = "none" })
-vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = border, bg = "none" })
-vim.api.nvim_set_hl(0, "FloatTitle", { fg = border, bg = "none" })
+vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = primary, bg = "none" })
+vim.api.nvim_set_hl(0, "FloatTitle", { fg = primary, bg = "none" })
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = "rounded", -- Add rounded border
+	primary = "rounded", -- Add rounded border
 })
-vim.api.nvim_set_hl(0, "WinSeparator", { fg = border, bg = "NONE" })
-vim.api.nvim_set_hl(0, "Pmenu", { fg = border, bg = "NONE" })
-vim.api.nvim_set_hl(0, "CmpBorder", { fg = border, bg = "NONE" })
-vim.api.nvim_set_hl(0, "PmenuThumb", { bg = border })
-vim.api.nvim_set_hl(0, "PmenuSel", { bg = border, fg = "#000000" })
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = primary, bg = "NONE" })
+vim.api.nvim_set_hl(0, "Pmenu", { fg = primary, bg = "NONE" })
+vim.api.nvim_set_hl(0, "CmpBorder", { fg = primary, bg = "NONE" })
+vim.api.nvim_set_hl(0, "PmenuThumb", { bg = primary })
+vim.api.nvim_set_hl(0, "PmenuSel", { bg = primary, fg = "#000000" })
 vim.api.nvim_set_hl(0, "LineNrAbove", { fg = "#506477" })
 vim.api.nvim_set_hl(0, "LineNr", { fg = "#DEDEDE" })
 vim.api.nvim_set_hl(0, "LineNrBelow", { fg = "#506477" })
